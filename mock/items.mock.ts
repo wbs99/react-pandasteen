a941a90da5f76e718105e7da4e0e334ad3e0811d
dingtalk_xsgrpl <1134954328@qq.com>
2022-11-28 10:08:59 +0800

    引入 faker.js

--- mock/items.mock.ts
+++ mock/items.mock.ts
@@ -1,4 +1,5 @@
 import { MockMethod } from 'vite-plugin-mock';
+import { faker } from '@faker-js/faker';
 
 export const MockItems: MockMethod = {
   url: '/api/v1/items',
@@ -19,11 +20,11 @@ const createItem = (attrs?: Partial<Item>): Item => {
   return {
     id: createId(),
     user_id: 1,
-    amount: 1000,
+    amount: faker.datatype.number({ min: 99, max: 1000_00 }),
     tag_ids: [1, 2],
-    happen_at: '2021-08-01T00:00:00.000Z',
-    created_at: '2021-08-01T00:00:00.000Z',
-    updated_at: '2021-08-01T00:00:00.000Z',
+    happen_at: faker.date.past().toISOString(),
+    created_at: faker.date.past().toISOString(),
+    updated_at: faker.date.past().toISOString(),
     kind: 'expenses',
     ...attrs
   }
