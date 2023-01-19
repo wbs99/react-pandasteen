b9db8f9bf33fa21240eb5f06f397dc936d1c31ef
dingtalk_xsgrpl <1134954328@qq.com>
2023-01-19 10:37:50 +0800

    完善 Tabs 组件，支持自定义 Tabs 水平布局方向

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,24 +1,40 @@
+import { ReactNode } from 'react'
 import s from './Tabs.module.scss'
+import cs from 'classnames'
+
 type Props<T> = {
   tabItems: {
     key: T
     text: string
+    element: ReactNode
   }[]
   value: T
   onChange: (key: T) => void
+  className?: string
+  classPrefix?: string
 }
 
 export const Tabs = <T extends string>(props: Props<T>) => {
-  const { tabItems, value, onChange } = props
+  const { tabItems, value, onChange, className, classPrefix } = props
 
   return (
-    <ol flex text-white children-px-24px children-py-12px >
-      {
-        tabItems.map(item => <li key={item.key} className={item.key === value ? s.selected : ''}
-          onClick={() => onChange(item.key)}>
-          {item.text}
-        </li>)
-      }
-    </ol>
+    <div className={cs(className, classPrefix)}>
+      <ol flex text-white children-px-24px children-py-12px bg='[rgb(143,76,215)]'
+        className={classPrefix ? `${classPrefix}-tabs` : ''}>
+        {
+          tabItems.map(item => <li key={item.key} className={
+            cs(item.key === value ? s.selected : '',
+              classPrefix ? `${classPrefix}-tabs-item` : ''
+            )
+          }
+            onClick={() => onChange(item.key)}>
+            {item.text}
+          </li>)
+        }
+      </ol>
+      <div className={classPrefix ? `${classPrefix}-pane` : ''}>
+        {tabItems.find(item => item.key === value)?.element}
+      </div>
+    </div >
   )
 }
