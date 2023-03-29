ef8d2311701d07ffd2c793566e5c23e35b6f9da1
wbs <1134954328@qq.com>
2023-03-29 22:29:47 +0800

    实现发送验证码倒计时

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react"
+
 type Props = {
   value?: string
   placeholder?: string
@@ -5,18 +7,47 @@ type Props = {
   request: () => Promise<unknown>
 }
 
+const maxCount = 60
 export const SmsCodeInput = (props: Props) => {
   const { value, placeholder, onChange, request } = props
+  const [started, setStarted] = useState<Date>()
+  const [count, setCount] = useState(maxCount)
+  const timer = useRef<number>()
+
   const onClick = async () => {
     if (!request) { return }
     await request()
+    setStarted(new Date())
   }
+  const clearTimer = () => {
+    if (timer.current) {
+      window.clearInterval(timer.current)
+      timer.current = undefined
+    }
+  }
+  useEffect(() => {
+    if (!started) {
+      clearTimer()
+      return
+    }
+    timer.current = window.setInterval(() => {
+      const seconds = Math.round((new Date().getTime() - started.getTime()) / 1000)
+      const count = maxCount - seconds
+      if (count < 0) { setStarted(undefined) }
+      setCount(count)
+    }, 1000)
+    return clearTimer
+  }, [started])
 
   return (
     <div flex gap-x-16px>
       <input max-w='[calc(40%-8px)]' p-input-text type="text" placeholder={placeholder}
         value={value} onChange={e => onChange?.(e.target.value)} />
-      <button type='button' max-w='[calc(60%-8px)]' p-btn onClick={onClick}>发送验证码</button>
+      {
+        started
+          ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 p-btn disabled text-gray>{count}秒后可重发</button>
+          : <button type='button' max-w='[calc(60%-8px)]' p-btn onClick={onClick}>发送验证码</button>
+      }
     </div>
   )
 }
