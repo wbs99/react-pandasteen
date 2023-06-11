593eccd8e414a1bc5efe626de8cd72f7747453c0
wbs <1134954328@qq.com>
2023-06-11 09:14:55 +0800

    修复路由报错，删除 .github 目录

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import { RouterProvider } from 'react-router-dom'
 import './global.scss'
 import 'virtual:uno.css'
 import './app.scss'
