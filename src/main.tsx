8952d2f537f6403082db9b000d372486b0c93aec
wbs <1134954328@qq.com>
2023-11-03 17:12:21 +0800

    Remove ESLint and update app styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import 'virtual:svgsprites'
 import 'virtual:uno.css'
-import './app.scss'
+import './assets/styles/app.scss'
 import { router } from './routes/router'
 
 const div = document.getElementById('root') as HTMLElement
