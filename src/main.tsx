15809deb2d8806de3d54942fa130de014f867f28
wbs <1134954328@qq.com>
2024-02-04 10:20:41 +0800

    引入 daisyui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/router'
 import 'virtual:svgsprites'
-import './assets/styles/tailwind.scss'
 import './assets/styles/global.scss'
+import './assets/styles/tailwind.scss'
+import { router } from './routes/router'
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
