1aba18db7e84931ed0b3f42566c7e0d39ee2bab6
wbs <1134954328@qq.com>
2025-02-07 11:41:20 +0800

    解决冲突

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,5 +1,10 @@
 import type { MockMethod } from 'vite-plugin-mock'
+import { mockItems } from './mockItems'
+import { mockMe } from './mockMe'
+import { mockSession } from './mockSession'
+import { mockSummary } from './mockSummary'
+import { mockTags } from './mockTags'
 
 export default [
-  // ...mockMe, ...mockItems, ...mockSession, ...mockTags, ...mockSummary
+  ...mockMe, ...mockItems, ...mockSession, ...mockTags, ...mockSummary
 ] as MockMethod[]
